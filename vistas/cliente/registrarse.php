21cea081d97db3fb563c6e85eaffeb52c21ed3ac
Gissela25 <gisselaverenice@gmail.com>
2022-02-28 22:39:34 -0600

    correcciones

--- vistas/cliente/registrarse.php
+++ vistas/cliente/registrarse.php
@@ -12,97 +12,65 @@
     </script>
     <link rel="stylesheet" href="../../Estilos/css/registro_cliente.css">
 </head>
+
 <body>
 
-<div class="container">
+    <div class="container">
     </div>
-    <nav class="navbar navbar-expand-lg navbar-dark"
-    style="background-color: #2d3585">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><i class="bi bi-display-fill"></i></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="">Regresar</a>
-                    </li>
-                </ul>
+
+
+    <div class="row justify-content-center pt-5 mt-5 mr-1">
+        <div class="col-md-4 formulario">
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Nombre" aria-label="First name">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Apellido" aria-label="Last name">
+                </div>
             </div>
         </div>
-    </nav>
+    </div>
+    <div class="row justify-content-center pt-4 mt-4 mr-1">
+        <div class="col-md-4 formulario">
 
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Direccion" aria-label="First name">
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center pt-4 mt-4 mr1">
+        <div class="col-md-4 formulario">
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Telefono" aria-label="First name">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="@mail.com" aria-label="Last name">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center pt-4 mt-4 mr-1">
+        <div class="col-md-4 formulario">
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Password" aria-label="First name">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Confirmar password" aria-label="Last name">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center pt-4 mt-4 mr-1">
+        <div class="col-md-4 formulario">
+            <div class="d-grid gap-2">
+                <button class="btn btn-warning" type="button">INGRESAR</button>
 
-
-
-<div class="row justify-content-center pt-4 mt-4 mr-1">
-            <div class="col-md-4 formulario">
-<div class="row" >
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Nombre" aria-label="First name">
-  </div>
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Apellido" aria-label="Last name">
-  </div>
-</div>
-</div>
-</div>
-<div class="row justify-content-center pt-4 mt-4 mr-1">
-<div class="col-md-4 formulario">
-
-<div class="col">
-    <input type="text" class="form-control" placeholder="Direccion" aria-label="First name">
-  
-
-    
-  
-  </div>
-  </div>
-  </div>
-  
-
-
-
-  <div class="row justify-content-center pt-4 mt-4 mr-1">
-            <div class="col-md-4 formulario">
-<div class="row" >
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Telefono" aria-label="First name">
-  </div>
-  <div class="col">
-    <input type="text" class="form-control" placeholder="@mail.com" aria-label="Last name">
-  </div>
-</div>
-</div>
-</div>
-
-
-<div class="row justify-content-center pt-4 mt-4 mr-1">
-            <div class="col-md-4 formulario">
-<div class="row" >
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Password" aria-label="First name">
-  </div>
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Confirmar password" aria-label="Last name">
-  </div>
-</div>
-</div>
-</div>
-
-
-<div class="row justify-content-center pt-4 mt-4 mr-1">
-            <div class="col-md-4 formulario">
-<div class="d-grid gap-2">
-  <button class="btn btn-warning" type="button">INGRESAR</button>
-  
-</div>
-</div>
-</div>
-
-
-    
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
--- vistas/empleados/registrarse.php
+++ vistas/empleados/registrarse.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,117 +13,67 @@
     </script>
     <link rel="stylesheet" href="../../Estilos/css/registro_empleado.css">
 </head>
+
 <body>
-    
-
-
-<div class="container">
-    </div>
-    <nav class="navbar navbar-expand-lg navbar-dark"
-    style="background-color: #2d3585">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><i class="bi bi-display-fill"></i></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="">Regresar</a>
-                    </li>
-                </ul>
+<div class="row justify-content-center pt-5 mt-5 mr-1">
+        <div class="col-md-4 formulario">
+            <div class="row">
+                <div class="col">
+                    <input type="text" color="Y" class="form-control" placeholder="Nombre" aria-label="First name">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Apellido" aria-label="Last name">
+                </div>
             </div>
         </div>
-    </nav>
-
-
-<div class="row justify-content-center pt-5 mt-5 mr-1">
-            <div class="col-md-4 formulario">
-
-
-      
-            
-
-
-            
-<div class="row" >
-  <div class="col">
-    <input type="text" color="Y" class="form-control" placeholder="Nombre" aria-label="First name">
-  </div>
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Apellido" aria-label="Last name">
-  </div>
-</div>
-</div>
-</div>
-<div class="row justify-content-center pt-5 mt-5 mr-1">
-<div class="col-md-4 formulario">
-
-<div class="col">
-    
-<div class="col-md">
-    <div class="form-floating">
-      <select class="form-select" id="floatingSelectGrid" aria-label="Floating label select example">
-        <!-- <option selected>Sucursal</option> -->
-        <option value="1">Alemania</option>
-        <option value="2">Santa Tecla</option>
-        <option value="3">Lourdes</option>
-        <option value="3">Opico</option>
-        <option value="3">Zaragoza</option>
-        <option value="3">Aldea</option>
-      </select>
-      <label for="floatingSelectGrid">SUCURSAL</label>
     </div>
-  </div>
-</div>
+    <div class="row justify-content-center pt-3 mt-3 mr-1">
+        <div class="col-md-4 formulario">
+            <div class="col">
+                <div class="col-md">
+                    <div class="form-floating">
+                        <select class="form-select" id="floatingSelectGrid" aria-label="Floating label select example">
+                            <option value="1">Alemania</option>
+                            <option value="2">Santa Tecla</option>
+                            <option value="3">Lourdes</option>
+                            <option value="4">Opico</option>
+                            <option value="5">Zaragoza</option>
+                            <option value="6">Aldea</option>
+                        </select>
+                        <label for="floatingSelectGrid">SUCURSAL</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center pt-3 mt-3 mr-1">
+        <div class="col-md-4 formulario">
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Puesto" aria-label="First name">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center pt-3 mt-3 mr-1">
+        <div class="col-md-4 formulario">
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Password" aria-label="First name">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Confirmar password" aria-label="Last name">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center pt-4 mt-4 mr-1">
+        <div class="col-md-4 formulario">
+            <div class="d-grid gap-2">
+                <button class="btn btn-warning" type="button">INGRESAR</button>
 
-
-
-
-  </div>
-  </div>
-  
-
-
-
-  <div class="row justify-content-center pt-4 mt-4 mr-1">
-            <div class="col-md-4 formulario">
-<div class="row" >
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Puesto" aria-label="First name">
-  </div>
-  <!-- <div class="col">
-    <input type="text" class="form-control" placeholder="@mail.com" aria-label="Last name">
-  </div> -->
-</div>
-</div>
-</div>
-
-
-
-
-
-<div class="row justify-content-center pt-4 mt-4 mr-1">
-            <div class="col-md-4 formulario">
-<div class="row" >
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Password" aria-label="First name">
-  </div>
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Confirmar password" aria-label="Last name">
-  </div>
-</div>
-</div>
-</div>
-
-<div class="row justify-content-center pt-4 mt-4 mr-1">
-            <div class="col-md-4 formulario">
-<div class="d-grid gap-2">
-  <button class="btn btn-warning" type="button">INGRESAR</button>
-  
-</div>
-</div>
-</div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
