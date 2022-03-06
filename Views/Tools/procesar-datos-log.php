9a5d59b478d2af9ecf314f0cab687335755319f8
Lord-Jony <jony25lopezml@gmail.com>
2022-03-05 19:33:34 -0600

    Login - Verificar Encriptado - Actualización BD : Leer descripción
    Añadí el campo Activo para la tabla 'cliente,'  y  agregué unos registros de prueba

--- Views/Tools/procesar-datos-log.php
+++ Views/Tools/procesar-datos-log.php
@@ -8,19 +8,24 @@
         {
             array_push($errores_log,"Debes ingresar una dirección de correo");
         }
-        if(!isset($i_pass)||isVoid($i_pass))
-        {
-            array_push($errores_log,"Debes ingresar una contraseña");
-        }elseif(!isMail($i_email))
+        elseif(!isMail($i_email))
         {
             array_push($errores_log,"Debes ingresar una dirección de correo válida");
         }
-        if(isMail(isset($i_email))&&isPass(isset($i_pass)))
+        if(!isset($i_pass)||isVoid($i_pass))
         {
-            $pwd_encrypt=  password_hash($i_email,PASSWORD_DEFAULT, ['cost' => 10]);    
-            $model = new Log();
-            $model->setEmail($i_email);
-            $model->setPass($pwd_encrypt);
-            $model->Login($i_email);
+            array_push($errores_log,"Debes ingresar una contraseña");
+        }
+        elseif(!isPass($i_pass))
+        {
+            array_push($errores_log,"Debes ingresar una contraseña válida");
+        }
+        if(isMail($i_email)&&isPass($i_pass))
+        {          
+            //$pwd_encrypt=  password_hash($i_email,PASSWORD_DEFAULT, ['cost' => 10]);    
+            $modelo = new User();
+            $modelo->setEmail($i_email);
+            $modelo->setPass($i_pass);
+            $modelo->Into();
         }
     }
