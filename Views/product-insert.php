0d299804e46ae2df6eae8e24b91fe971a99170c2
portan17 <william.portan1415@gmail.com>
2022-03-10 22:27:34 -0600

    add views

--- Views/product-insert.php
+++ Views/product-insert.php
@@ -5,21 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sucursales</title>
+    <title>Producto</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
-    <link rel="stylesheet" href="../Assets/css/worker.css">
+    <link rel="stylesheet" href="../Assets/css/product.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 </head>
 
 <body>
-    <div class="d-flex justify-content-center">
+<div class="d-flex justify-content-center">
         <div class="col-md-4 my-5">
-            <legend style="color:#084594" class="text-center">Informacion Empleado</legend>
-            <form class="formulario" method="POST" action="?c=branch&a=Save">
+            <legend style="color:#084594" class="text-center">Informacion Producto</legend>
+            <form class="formulario" method="POST" action="?c=branch&a=Save" >
                 <img src="Assets/img/logo.PNG" class="rounded mx-auto d-block mb-3" alt="..." height="85px"
                     width="230px">
                 <div class="form-group">
@@ -28,36 +28,41 @@
                     </div>
                 </div>
                 <div class="mb-3" style="color:#084594">
-                    <label for="exampleInputPassword1" class="form-label">Nombre</label>
-                    <input type="name" class="form-control" name="Nombre">
+                    <label for="exampleInputPassword1" class="form-label">Codigo</label>
+                    <input type="name" class="form-control" name="Nombre" >
                 </div>
                 <div class="mb-3" style="color:#084594">
-                    <label for="exampleInputPassword1" class="form-label">Apellido</label>
+                    <label for="exampleInputPassword1" class="form-label">Nombre</label>
                     <input type="name" class="form-control" name="Apellido">
                 </div>
                 <div class="mb-3" style="color:#084594">
-                    <label for="exampleInputPassword1" class="form-label">Clave</label>
+                    <label for="exampleInputPassword1" class="form-label">Descripcion</label>
                     <input type="name" class="form-control" name="Clave">
                 </div>
-                <div class="form-floating">
-                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-                <option value="1">Santa Tecla</option>
-                <option value="2">Alemania</option>
-                <option value="3">Zaragoza</option>
-                <option value="4">Lourdes</option>
-                <option value="5">Opico</option>
-                <option value="6">Aldea</option>
-                </select>
-                <label for="floatingSelect">Sucursal</label>
+                <div class="mb-3" style="color:#084594" >
+                    <label for="exampleInputPassword1" class="form-label">Categoria</label>
+                    <input type="name" class="form-control" name="Clave">
                 </div>
-                 <div class="d-flex justify-content-center">
+                <div class="mb-3" style="color:#084594">
+                    <label for="exampleInputPassword1" class="form-label">Precio</label>
+                    <input type="name" class="form-control" name="Clave">
+                </div>
+                <div class="mb-3" style="color:#084594">
+                    <label for="exampleInputPassword1" class="form-label">Existencia</label>
+                    <input type="name" class="form-control" name="Clave">
+                </div>
+                <div class="d-flex justify-content-center">
                  <div class="col-md-3 my-4">
                 <button type="submit" class="btn btn-warning">Ingresar</button>
-            </form>
+            
             </div>
             </div>
             
-    </div>
+            
+            
+            </div>
+
+                
 </body>
 
 </html>
