eb81b2b0d5e1ab848f506b7d28886f87201f390c
susan749 <susanzelaya15@gmail.com>
2022-03-10 19:37:45 -0600

    Create Registrar.php

---
+++ Views/Registrar.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body class="bg-secondary">
+<section class="d-flex justify-content-center">
+         <div class="card col-sm-4 p-5">
+        <div class="nb-3">
+            <h4>Resgistar</h4>
+        </div>
+        <div class="nd-2">
+            <form action="">
+                <div class="nd-2">
+                    <label for="dui">DUI</label>
+                    <input type="text"  class="form-control col-xs-6" name="nombre" id="nombre" placeholder="Ingrese su número de DUI">
+                </div>
+                <div class="nd-2">
+                    <label for="nombre">Nombre</label>
+                    <input type="text"  class="form-control col-xs-6" name="nombre" id="nombre" placeholder="Ingrese su nombre">
+                </div>
+                <div class="nd-2">
+                    <label for="apellido">Apellido</label>
+                    <input type="text"  class="form-control" name="nombre" id="nombre" placeholder="Ingrese su apellido">
+                </div>
+                <div class="nd-2">
+                    <label for="telefono">Teléfono</label>
+                    <input type="text"  class="form-control" name="nombre" id="nombre" placeholder="Ingrese su Número de teléfono">
+                </div>
+                <div class="nd-2">
+                    <label for="dirección">Dirección</label>
+                    <input type="text"  class="form-control" name="nombre" id="nombre" placeholder="Ingrese su dirección">
+                </div>
+        <div class="nd-2">
+                <div class="nd-2">
+                    <label for="correo">Correo</label>
+                    <input type="text"  class="form-control" name="nombre" id="nombre" placeholder="Ingrese su E-mail">
+                </div>
+                <div class="nd-2">
+                    <label for="contraseña">Contraseña</label>
+                    <input type="text"  class="form-control" name="nombre" id="nombre" placeholder="Ingrese su contraseña">
+                </div>
+                <div class="nd-2">
+                    <label for="verificar contraseña">Verificar contraseña</label>
+                    <input type="text"  class="form-control" name="nombre" id="nombre" placeholder="verificar contraseña">
+                </div>
+               
+                
+                <div class="nd-2">
+                    <button class="btn btn-primary">Registrarme</button>
+                </div>
+            </form>
+        </div>
+        </div>
+        </section>
+    
+</body>
+</html>
