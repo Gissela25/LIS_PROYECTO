91c4f7411823eceb828e70638001b29fe3e330ce
Gissela25 <gisselaverenice@gmail.com>
2022-03-14 01:33:24 -0600

    update edit

--- Views/worker-insert.php
+++ Views/worker-insert.php
@@ -11,25 +11,25 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
-    <link rel="stylesheet" href="../Assets/css/branch.css">
+    <link rel="stylesheet" href="Assets/css/worker-insert.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 </head>
 
 <body>
     <div class="d-flex justify-content-center">
         <div class="col-md-4 my-5">
-            <legend style="color:#084594" class="text-center"><?=$titulo?> Empleado</legend>
+            <img src="Assets/img/logo.PNG" class="rounded mx-auto d-block mb-3" alt="..." height="65px" width="200px">
             <form class="formulario" method="POST" action="?c=worker&a=Save">
-                <img src="Assets/img/logo.PNG" class="rounded mx-auto d-block mb-3" alt="..." height="85px"
-                    width="230px">
+                <legend style="color:#084594" class="text-center"><?=$titulo?> Empleado</legend>
                 <!-- <div class="form-group">
                     <div class="col-lg-10">
                         <input class="form-control" name="ID_Sucursal" type="hidden" value="">
                     </div>
                 </div> -->
                 <div class="mb-3" style="color:#084594">
-                    <label for="exampleInputPassword1" class="form-label" >ID Usuario</label>
-                    <input type="name" class="form-control" name="ID_Usuario" value="<?=$this->modelo->generate_code();?>" readonly>
+                    <label for="exampleInputPassword1" class="form-label">ID Usuario</label>
+                    <input type="name" class="form-control" name="ID_Usuario"
+                        value="<?=$this->modelo->generate_code();?>" readonly>
                 </div>
                 <div class="mb-3" style="color:#084594">
                     <label for="exampleInputPassword1" class="form-label">Nombre</label>
@@ -62,18 +62,21 @@
                         <input class="form-control" name="Acceso" type="hidden" value="">
                     </div>
                 </div>
-                <label for="exampleInputPassword1" class="form-label">Sucursal</label>
-                <div class="form-floating">
-                    <select class="form-select" name="ID_Sucursal" id="floatingSelect"
-                        aria-label="Floating label select example">
-                        <?php foreach($this->modelo->showsucursal() as $r):?>
-                        <option value="<?=$r->ID_Sucursal?>"><?=$r->Nombre_Sucursal?></option>
-                        <?php endforeach;?>
-                    </select>
-                </div>
-                <div class="d-flex justify-content-center">
-                    <div class="col-md-3 my-4">
-                        <button type="submit" class="btn btn-warning">Ingresar</button>
+                <div class="mb-3" style="color:#084594">
+                    <label for="exampleInputPassword1" class="form-label">Correo</label>
+                    <div class="form-floating">
+                        <select class="form-select" name="ID_Sucursal" id="floatingSelect"
+                            aria-label="Floating label select example">
+                            <?php foreach($this->modelo->showsucursal() as $r):?>
+                            <option value="<?=$r->ID_Sucursal?>"><?=$r->Nombre_Sucursal?></option>
+                            <?php endforeach;?>
+                        </select>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                    <div class="my-2">
+                            <input type="submit" class="btn "></input>
+                        </div>
+                        
             </form>
         </div>
     </div>
