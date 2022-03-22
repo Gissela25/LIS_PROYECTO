3c958ab960e32da56732b04ddf90ee74b97869ca
Gissela25 <55642333+Gissela25@users.noreply.github.com>
2022-03-21 21:15:37 -0600

    Registrar

--- Views/Registrar.php
+++ Views/Registrar.php
@@ -5,81 +5,117 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="showc.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <title>Recuperar Cuenta</title>
-    <!-- <link rel="stylesheet" href="../Assets/css/worker-insert.css"> -->
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" />
+    <title>Empleados</title>
 </head>
 
 <body>
-    <div class="d-flex justify-content-center">
-        <div class="col-md-10 my-5">
-            <img src="../Assets/img/logo.PNG" class="rounded mx-auto d-block mb-3" alt="..." height="65px"
-                width="200px">
-            <!-- <div class="card col-sm-4 p-5"> -->
-            <!-- <div class="nb-3">
-                <h4>Resgistar</h4>
-            </div>
-            <div class="nd-2"> -->
-            <form class="" method="POST" action="">
-                <div class="col-xs-6">
-                    <h1 style="color:#084594">Informacion</h1>
-                    <label for="nombre" style="color:#084594">Nombre</label>
-                    <input type="text" class="form-control" />
-                </div>
-                <div class="col-xs-6">
-                    <h1 style="color:#084594">Informacion</h1>
-                    <label for="nombre" style="color:#084594">Nombre</label>
-                    <input type="text" class="form-control" />
-                </div>
-                <!-- <div class="nd-2">
 
-                    <input type="text" class="form-control col-xs-6" name="nombre" id="nombre"
-                        placeholder="Ingrese su número de DUI">
-                </div> -->
-                <!-- <div class="nd-2">
-                    <label for="nombre">Nombre</label>
-                    <input type="text" class="form-control col-xs-6" name="nombre" id="nombre"
-                        placeholder="Ingrese su nombre">
-                </div>
-                <div class="nd-2">
-                    <label for="apellido">Apellido</label>
-                    <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Ingrese su apellido">
-                </div>
-                <div class="nd-2">
-                    <label for="telefono">Teléfono</label>
-                    <input type="text" class="form-control" name="nombre" id="nombre"
-                        placeholder="Ingrese su Número de teléfono">
-                </div>
-                <div class="nd-2">
-                    <label for="dirección">Dirección</label>
-                    <input type="text" class="form-control" name="nombre" id="nombre"
-                        placeholder="Ingrese su dirección">
-                </div>
-                <div class="nd-2">
-                    <div class="nd-2">
-                        <label for="correo">Correo</label>
-                        <input type="text" class="form-control" name="nombre" id="nombre"
-                            placeholder="Ingrese su E-mail">
-                    </div>
-                    <div class="nd-2">
-                        <label for="contraseña">Contraseña</label>
-                        <input type="text" class="form-control" name="nombre" id="nombre"
-                            placeholder="Ingrese su contraseña">
-                    </div>
-                    <div class="nd-2">
-                        <label for="verificar contraseña">Verificar contraseña</label>
-                        <input type="text" class="form-control" name="nombre" id="nombre"
-                            placeholder="verificar contraseña">
-                    </div> -->
-                <button type="button" class="btn btn-outline-primary">Primary</button>
-            </form>
-        </div>
-    </div>
-</body>
+    <div class="row mx-5 mt-5 my-4">
+        <div class="col ml-5">
+            <div class="row mt-3">
+
+                <table class="table table-borderless">
+                    <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Informacion Personal</th>
+                            <th scope="col"></th>
+                            <th scope="col">
+                                DATOS DE INICIO DE SESIÓN</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row"></th>
+                            <td>
+                                <div class="form-group mx-sm-4 pt-3">
+                                    <label for="exampleInputPassword1" class="form-label">DUI</label>
+                                    <input type="text" class="form-control" placeholder="Ingrese su Correo Electronico"
+                                        name="i_email" id="i_email">
+                                </div>
+                            </td>
+                            <td></td>
+                            <td>
+                                <div class="form-group mx-sm-4 pt-3">
+                                    <label for="exampleInputPassword1" class="form-label">Correo</label>
+                                    <input type="text" class="form-control" placeholder="Ingrese su Correo Electronico"
+                                        name="i_email" id="i_email">
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td>
+                                <div class="form-group mx-sm-4 pt-3">
+                                    <label for="exampleInputPassword1" class="form-label">Nombre</label>
+                                    <input type="text" class="form-control" placeholder="Ingrese su Correo Electronico"
+                                        name="i_email" id="i_email">
+                                </div>
+                            </td>
+                            <td></td>
+                            <td>
+                                <div class="form-group mx-sm-4 pt-3">
+                                    <label for="exampleInputPassword1" class="form-label">Clave</label>
+                                    <input type="text" class="form-control" placeholder="Ingrese su Correo Electronico"
+                                        name="i_email" id="i_email">
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td>
+                                <div class="form-group mx-sm-4 pt-3">
+                                    <label for="exampleInputPassword1" class="form-label">Apellido</label>
+                                    <input type="text" class="form-control" placeholder="Ingrese su Correo Electronico"
+                                        name="i_email" id="i_email">
+                                </div>
+                            </td>
+                            <td></td>
+                            <td>
+                                <div class="form-group mx-sm-4 pt-3">
+                                    <label for="exampleInputPassword1" class="form-label">Correo</label>
+                                    <input type="text" class="form-control" placeholder="Ingrese su Correo Electronico"
+                                        name="i_email" id="i_email">
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td>
+                                <div class="form-group mx-sm-4 pt-3">
+                                    <label for="exampleInputPassword1" class="form-label">DUI</label>
+                                    <input type="text" class="form-control" placeholder="Ingrese su Correo Electronico"
+                                        name="i_email" id="i_email">
+                                </div>
+                            </td>
+                            <td></td>
+                            <td>
+                                <a href="" class="btn btn-primary btn-block boton mx-5">Ver
+                                    más</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td>
+                                <div class="form-group mx-sm-4 pt-3">
+                                    <label for="exampleInputPassword1" class="form-label">DUI</label>
+                                    <input type="text" class="form-control" placeholder="Ingrese su Correo Electronico"
+                                        name="i_email" id="i_email">
+                                </div>
+                            </td>
+                            <td></td>
+                            <td>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>a
 
 </html>
