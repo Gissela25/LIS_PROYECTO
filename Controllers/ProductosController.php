6a9bc60dd0ad2d39cff8949fed5a0b5715a5db2f
Jony-07 <jony25lopezml@gmail.com>
2022-05-05 14:41:02 -0600

    etc

--- Controllers/ProductosController.php
+++ Controllers/ProductosController.php
@@ -430,9 +430,15 @@
         public function Detalles($var,$id)
         {
             $viewBag=[];
+            if(isset($_SESSION['login_buffer']))
+            {
+                if($_SESSION['login_buffer']['Acceso']==2)
+                {
             $carritosModel = new CarritosModel();
             $id_carrito=sha1($_SESSION['login_buffer']['DUI']);
             $viewBag['carritos']=$carritosModel->CountQuantity($id_carrito);
+                }
+            }
             $viewBag['productos']=$this->modelo->getByID($var,$id);
             $this->render("Clientes/detalles.php",$viewBag);
         }
